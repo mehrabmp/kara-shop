39347687efe9d28b0c9fc376a69c49bab2577fde
MehrabMp <mehrabmpdev@gmail.com>
2022-07-23 00:43:26 +0430

    chore: Install Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-addon-next',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  staticDirs: ['../public'],
+};
