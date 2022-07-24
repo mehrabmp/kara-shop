0c18382b8070406ee9ad985f9c1bf5da4fa3dcfc
MehrabMp <mehrabmpdev@gmail.com>
2022-07-24 15:46:33 +0430

    feat: Update storybook to use Theme and GlobalStyles in all stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,22 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../src/styles/GlobalStyles';
+import { lightTheme } from '../src/styles/Theme';
+
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
