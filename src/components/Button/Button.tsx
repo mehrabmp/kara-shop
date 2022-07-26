a416539b6048c8af73d93857083729f5f32d05f8
MehrabMp <mehrabmpdev@gmail.com>
2022-07-26 15:01:43 +0430

    refactor: refactor Button component and update styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import styled, { useTheme, DefaultTheme } from 'styled-components';
-import { getTextColorByContrast, LightenDarkenColor } from 'utils';
+import styled from 'styled-components';
 
 export interface Props {
   /** Button text */
   label: string;
   /** Button color variants */
-  variant?: keyof DefaultTheme['colors'];
+  variant?: 'primary' | 'secondary' | 'tertiary';
   /** Button with full width */
-  block?: boolean;
+  full?: boolean;
+  /** Disabled button */
+  disabled?: boolean;
   /** Type of button */
   type?: 'button' | 'submit' | 'reset' | undefined;
   /** Optional children elements */
@@ -21,19 +22,19 @@ export const Button: React.FC<Props> = ({
   label,
   children,
   variant = 'primary',
-  block = false,
+  full = false,
+  disabled = false,
   type,
   onClick,
   ...props
 }) => {
-  const themeContext = useTheme();
-
   return (
     <StyledButton
-      color={themeContext.colors[variant]}
-      block={block}
+      variant={variant}
+      full={full}
       type={type}
       onClick={onClick}
+      disabled={disabled}
       {...props}
     >
       {label || children}
@@ -42,20 +43,44 @@ export const Button: React.FC<Props> = ({
 };
 
 interface ButtonProps {
-  color: string;
-  block: boolean;
+  variant: string;
+  full: boolean;
 }
 
 const StyledButton = styled.button<ButtonProps>`
-  width: ${({ block }) => (block ? '100%' : 'auto')};
-  background-color: ${({ color }) => color};
-  color: ${({ color }) => getTextColorByContrast(color)};
+  width: ${({ full }) => (full ? '100%' : 'auto')};
+  background-color: ${({ theme, variant }) =>
+    variant === 'primary'
+      ? theme.colors.primary100
+      : variant === 'secondary'
+      ? theme.colors.neutral10
+      : theme.colors.tertiary};
+  border: ${({ theme, variant }) =>
+    variant === 'tertiary' && `2px solid ${theme.colors.neutral10}`};
+  color: ${({ theme, variant }) =>
+    variant === 'primary' ? theme.colors.neutral00 : theme.colors.neutral80};
   cursor: pointer;
   font-size: 1.6rem;
-  font-weight: 400;
+  font-weight: 500;
   transition: background-color 0.3s;
+  outline-color: ${({ theme }) => theme.colors.neutral90};
+  outline-width: thin;
 
-  &:hover {
-    background-color: ${({ color }) => LightenDarkenColor(color, -40)};
+  &:hover,
+  &:active {
+    background-color: ${({ theme, variant }) =>
+      variant === 'primary'
+        ? theme.colors.primary140
+        : variant === 'secondary'
+        ? theme.colors.neutral20
+        : theme.colors.neutral10};
+    border: ${({ theme, variant }) =>
+      variant === 'tertiary' ? `2px solid ${theme.colors.neutral20}` : 'none'};
+  }
+
+  &:disabled {
+    color: ${({ theme }) => theme.colors.neutral30};
+    border: ${({ theme }) => `2px solid ${theme.colors.neutral10}`};
+    background-color: ${({ theme }) => theme.colors.neutral05};
   }
 `;
