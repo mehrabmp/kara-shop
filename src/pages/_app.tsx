e1fbae0b85ba8b4cf65e0256d0cf4ce473f940dc
MehrabMp <mehrabmpdev@gmail.com>
2022-07-26 15:57:19 +0430

    feat: remove dark mode option temporarily

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from 'styles/Theme';
+import { lightTheme } from 'styles/Theme';
 import GlobalStyles from 'styles/GlobalStyles';
-import useDarkMode from 'hooks/useDarkMode';
+// import useDarkMode from 'hooks/useDarkMode';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { isDarkMode } = useDarkMode(false);
+  // const { isDarkMode } = useDarkMode(false);
 
   return (
-    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+    <ThemeProvider theme={lightTheme}>
       <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,15 @@
 import type { NextPage } from 'next';
 import styled from 'styled-components';
-import useDarkMode from 'hooks/useDarkMode';
-import { useEffect, useState } from 'react';
-import { Button } from 'components';
+import { TopBar } from 'components';
 
 const Home: NextPage = () => {
-  const { isDarkMode, toggle } = useDarkMode(false);
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    setDarkMode(isDarkMode);
-  }, [isDarkMode]);
-
   return (
     <Container>
-      <h1>Its test</h1>
-      <Button label={darkMode ? 'Light mode' : 'Dark mode'} onClick={toggle} />
+      <TopBar></TopBar>
     </Container>
   );
 };
 
 export default Home;
 
-const Container = styled.div`
-  padding: 1rem;
-`;
+const Container = styled.div``;
