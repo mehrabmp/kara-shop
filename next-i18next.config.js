454c3021748ddace7b5dff190f6329ce3d5b51df
MehrabMp <mehrabmpdev@gmail.com>
2022-08-08 00:48:27 +0430

    refactor: move locales to public folder

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
-
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
   },
-  localePath: path.resolve('./src/locales'),
 };
--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
