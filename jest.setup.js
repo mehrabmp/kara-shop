3129e2388f8f17d84e8069c6a73d5f60b93858c5
MehrabMp <mehrabmpdev@gmail.com>
2022-08-07 23:40:11 +0430

    fix: mock i18next in jest

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: str => str,
+      i18n: {
+        changeLanguage: () => new Promise(),
+      },
+    };
+  },
+}));
