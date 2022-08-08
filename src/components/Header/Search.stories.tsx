144055d4781a0e5f949927589b73fe545a4ddcfe
MehrabMp <mehrabmpdev@gmail.com>
2022-08-09 01:05:25 +0430

    fix: update Search component stories

--- src/components/Header/Search.stories.tsx
+++ src/components/Header/Search.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { screen, userEvent } from '@storybook/testing-library';
+// import { screen, userEvent } from '@storybook/testing-library';
 import { Search } from './Search';
 
 export default {
@@ -12,13 +12,13 @@ const Template: ComponentStory<typeof Search> = args => <Search {...args} />;
 
 export const Base = Template.bind({});
 
-export const Filled = Template.bind({});
-Filled.play = async () => {
-  const searchInput = screen.getByRole('textbox', {
-    name: /search on kara/i,
-  });
+// export const Filled = Template.bind({});
+// Filled.play = async () => {
+//   const searchInput = screen.getByRole('textbox', {
+//     name: /search on kara/i,
+//   });
 
-  await userEvent.type(searchInput, 'Black Jeans{enter}', {
-    delay: 100,
-  });
-};
+//   await userEvent.type(searchInput, 'Black Jeans{enter}', {
+//     delay: 100,
+//   });
+// };
