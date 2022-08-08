6836b2e9728881e4093613c3cb7f5b9853c470ec
MehrabMp <mehrabmpdev@gmail.com>
2022-08-08 23:33:45 +0430

    chore: update nextI18next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  // https://www.i18next.com/overview/configuration-options#logging
+  debug: false,
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
   },
   localePath: path.resolve('./public/locales'),
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
