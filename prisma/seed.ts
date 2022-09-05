0c7eec2b0ead1ffe80cc05e5851e0007c175eeba
MehrabMp <mehrabmpdev@gmail.com>
2022-09-05 14:42:59 +0430

    feat: update prisma seeder script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,18 @@
 import { PrismaClient } from '@prisma/client';
-import { collections } from '../src/data';
+import { collections, subCollections } from '../src/data';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const tablenames = await prisma.$queryRaw<
-    Array<{ tablename: string }>
-  >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
-
-  for (const { tablename } of tablenames) {
-    if (tablename !== '_prisma_migrations') {
-      try {
-        await prisma.$executeRawUnsafe(
-          `TRUNCATE TABLE "public"."${tablename}" CASCADE;`
-        );
-      } catch (error) {
-        console.log({ error });
-      }
-    }
-  }
-
   const createCollections = prisma.collection.createMany({
     data: collections,
   });
 
-  await prisma.$transaction([createCollections]);
+  const createSubCollections = prisma.subCollection.createMany({
+    data: subCollections,
+  });
+
+  await prisma.$transaction([createCollections, createSubCollections]);
 }
 
 main()
