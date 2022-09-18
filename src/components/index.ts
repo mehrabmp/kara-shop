7bab6e516e5ffd0c2644061e6ebf5e9b5f3ae07d
MehrabMp <mehrabmpdev@gmail.com>
2022-09-19 01:07:43 +0430

    refactor: drop storybook

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,3 @@
-export { BaseContainer } from './Header/TopBar';
 export * from './Layouts/Primary/PrimaryLayout';
 export * from './Header/Header';
 export * from './Footer/Footer';
-export * from './Button/Button';
