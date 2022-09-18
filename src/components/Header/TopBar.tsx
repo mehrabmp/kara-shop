55e0ce13d41791cb8cd12c7a901e41062c703e7b
MehrabMp <mehrabmpdev@gmail.com>
2022-09-18 22:05:44 +0430

    refactor: update TopBar component

--- src/components/Header/TopBar.tsx
+++ src/components/Header/TopBar.tsx
@@ -2,25 +2,25 @@ import React, { useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
-import styled from 'styled-components';
 import { useTranslation } from 'next-i18next';
 import { FiPhone, FiGrid, FiChevronDown } from 'react-icons/fi';
 import { ChangeLocale } from './ChangeLocale';
 import { useClickAway } from 'react-use';
+import { IconType } from 'react-icons';
 
 export const TopBar = () => {
-  const router = useRouter();
-
   const [isChangeLocaleOpen, setIsChangeLocaleOpen] = useState(false);
+
+  const router = useRouter();
 
   const { t } = useTranslation('header');
 
-  const topbarItems = [
-    { label: t('topbar.careers'), href: '#' },
-    { label: t('topbar.help'), href: '#' },
-    { label: t('topbar.buyer'), href: '#' },
-    { label: t('topbar.download'), href: '#', Icon: FiGrid },
-    { label: t('topbar.phone'), href: '#', Icon: FiPhone },
+  const topbarItems: [string, string, IconType?][] = [
+    [t('topbar.careers'), '#'],
+    [t('topbar.help'), '#'],
+    [t('topbar.buyer'), '#'],
+    [t('topbar.download'), '#', FiGrid],
+    [t('topbar.phone'), '#', FiPhone],
   ];
 
   const ref = useRef<HTMLDivElement>(null);
@@ -28,25 +28,29 @@ export const TopBar = () => {
   useClickAway(ref, () => setIsChangeLocaleOpen(false));
 
   return (
-    <StyledTopBar>
-      <Container>
-        <Title>{t('topbar.discount')}</Title>
-        <List>
-          {topbarItems.map(({ label, href, Icon }) => (
-            <ListItem key={label}>
-              <Link href={href}>
-                <a>
-                  {Icon && <Icon className="icon"></Icon>}
+    <div className="bg-[#232323] text-gray-300 text-[10px] md:text-xs">
+      <div className="mx-auto px-4 py-1 flex flex-col items-center md:flex-row md:py-2.5 xl:container">
+        <p className="pb-2 md:pb-0">{t('topbar.discount')}</p>
+        <ul className="flex justify-center flex-wrap md:ml-auto">
+          {topbarItems.map(([label, url, Icon]) => (
+            <li
+              key={label}
+              className="mx-1 pb-px md:mr-2.5 lg:[&:nth-of-type(3)]:mr-10 lg:[&:nth-of-type(5)]:mr-10"
+            >
+              <Link href={url}>
+                <a className="flex items-center hover:text-white transition-colors">
+                  {Icon && <Icon className="mx-1 md:text-sm"></Icon>}
                   <span>{label}</span>
                 </a>
               </Link>
-            </ListItem>
+            </li>
           ))}
-          <SelectLocale
+          <div
+            className="flex items-center cursor-pointer ml-2.5 relative"
             ref={ref}
             onClick={() => setIsChangeLocaleOpen(prev => !prev)}
           >
-            <div className="flag">
+            <div className="relative mr-1.5 w-3.5 h-3.5 md:w-[17px] md:h-[17px]">
               <Image
                 priority
                 src={`/assets/${router.locale}-flag.svg`}
@@ -57,112 +61,9 @@ export const TopBar = () => {
             <span>{router.locale?.toUpperCase()}</span>
             <FiChevronDown color="#fff"></FiChevronDown>
             <ChangeLocale isOpen={isChangeLocaleOpen} />
-          </SelectLocale>
-        </List>
-      </Container>
-    </StyledTopBar>
+          </div>
+        </ul>
+      </div>
+    </div>
   );
 };
-
-export const BaseContainer = styled.div`
-  max-width: ${({ theme }) => theme.containerWidth};
-  margin: 0 auto;
-  padding: 0 1rem;
-`;
-
-const StyledTopBar = styled.div`
-  background-color: #232323;
-  color: rgba(255, 255, 255, 0.8);
-  font-size: 1.2rem;
-`;
-
-const Container = styled(BaseContainer)`
-  padding: 1rem;
-  display: flex;
-
-  @media (max-width: 800px) {
-    flex-direction: column;
-    align-items: center;
-    padding: 0.5rem 1rem;
-    font-size: 1rem;
-  }
-`;
-
-const Title = styled.p`
-  @media (max-width: 800px) {
-    margin-bottom: 0.75rem;
-  }
-`;
-
-const List = styled.ul`
-  display: flex;
-  margin-left: auto;
-
-  @media (max-width: 800px) {
-    margin-left: 0;
-    justify-content: center;
-    flex-wrap: wrap;
-  }
-`;
-
-const ListItem = styled.li`
-  margin-right: 2rem;
-  transition: all 0.2s;
-
-  a {
-    display: flex;
-    align-items: center;
-  }
-
-  .icon {
-    margin: 0 0.5rem;
-    font-size: 1.4rem;
-  }
-
-  &:hover {
-    color: rgba(255, 255, 255, 1);
-  }
-
-  &:nth-of-type(3),
-  &:nth-of-type(5) {
-    margin-right: 5rem;
-  }
-
-  @media (max-width: 900px) {
-    &:nth-of-type(n) {
-      margin: 0 0.5rem;
-    }
-  }
-
-  @media (max-width: 800px) {
-    padding-bottom: 0.2rem;
-
-    .icon {
-      font-size: 1rem;
-    }
-  }
-`;
-
-const SelectLocale = styled.div`
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-  margin-left: 1rem;
-  position: relative;
-
-  .flag {
-    position: relative;
-    width: 17px;
-    height: 17px;
-    margin-right: 0.5rem;
-  }
-
-  @media (max-width: 800px) {
-    padding-bottom: 0.2rem;
-
-    .flag {
-      width: 14px;
-      height: 14px;
-    }
-  }
-`;
