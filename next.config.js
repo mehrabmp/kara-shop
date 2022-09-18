a4b2f88054775f0571a3dbee138ada36d492a42d
MehrabMp <mehrabmpdev@gmail.com>
2022-09-18 22:09:34 +0430

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
 const { i18n } = require('./next-i18next.config');
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
