f43c55b55ec39720e2f90b1cdb47ace06a2eb8ba
MehrabMp <mehrabmpdev@gmail.com>
2022-09-23 22:21:45 +0330

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,11 @@ const createJestConfig = nextJest({
   dir: './',
 });
 
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
 const customJestConfig = {
   // Add more setup options before each test is run
-  setupFilesAfterEnv: ['./jest.setup.js'],
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', 'src/'],
   testEnvironment: 'jest-environment-jsdom',
