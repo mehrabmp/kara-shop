28eb4125dd03bcd3cd6b9be7f0758068338a6d5f
mehrabmp <mehrabmpdev@gmail.com>
2022-09-30 21:49:19 +0330

    refactor: drop unused types

--- src/types/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import { lightTheme } from 'styles/Theme';
-
-type Theme = typeof lightTheme;
-
-declare module 'styled-components' {
-  // eslint-disable-next-line
-  export interface DefaultTheme extends Theme {}
-}
