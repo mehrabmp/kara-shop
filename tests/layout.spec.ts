3d5e9645253a95001ada4b7e1329d77be6ba28b6
Mehrabmp <mehrabmpdev@gmail.com>
2022-10-18 00:18:25 +0330

    refactor(tests): update layout tests

--- tests/layout.spec.ts
+++ tests/layout.spec.ts
@@ -1,60 +1,79 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
 });
 
+const TOPBAR_LINKS = [
+  ['Careers', '/careers'],
+  ['Help', '/help'],
+  ['Buyer Protection', '/buyer'],
+  ['Download Mobile App', 'https://play.google.com/store/apps'],
+];
+
+const MENU_ITEMS = ['Men', 'Women', 'Kids', 'Sale', 'Blog', 'Contacts'];
+
 test.describe('Header', () => {
-  test('should change language to De', async ({ page }) => {
-    await page.getByRole('img', { name: 'en locale' }).click();
+  test.describe('Top Bar', () => {
+    test('Links in the top bar should redirect to the correct page', async ({
+      page,
+    }) => {
+      for (const item of TOPBAR_LINKS) {
+        await testLink(page, item[0], item[1]);
+        await page.goBack();
+      }
+    });
 
-    await page.getByRole('link', { name: 'de locale German' }).click();
-    await expect(page).toHaveURL('/de');
+    test('Change website language to de', async ({ page }) => {
+      await page.getByRole('img', { name: 'en locale' }).click();
 
-    await expect(page.getByRole('link', { name: 'Hilfe' })).toBeVisible();
-    await expect(page.getByPlaceholder('Suche')).toBeEditable();
-    await expect(page.getByRole('link', { name: 'Kinder' })).toBeVisible();
+      await page.getByRole('link', { name: 'de locale German' }).click();
+      await expect(page).toHaveURL('/de');
+
+      await expect(page.getByRole('link', { name: 'Hilfe' })).toBeVisible();
+      await expect(page.getByPlaceholder('Suche')).toBeEditable();
+      await expect(page.getByRole('link', { name: 'Kinder' })).toBeVisible();
+    });
+
+    test('Change website language to en', async ({ page }) => {
+      await page.goto('/de');
+
+      await page.getByRole('img', { name: 'de locale' }).click();
+
+      await page.getByRole('link', { name: 'en locale English' }).click();
+      await expect(page).toHaveURL('/');
+
+      await expect(page.getByRole('link', { name: 'Help' })).toBeVisible();
+      await expect(page.getByPlaceholder('Search')).toBeEditable();
+      await expect(page.getByRole('link', { name: 'Kids' })).toBeVisible();
+    });
   });
 
-  test('should change language to En', async ({ page }) => {
-    await page.goto('/de');
+  test.describe('Menu', () => {
+    test('Should clear search box when the clear icon clicked', async ({
+      page,
+    }) => {
+      await page.getByPlaceholder('Search').fill('Jeans');
 
-    await page.getByRole('img', { name: 'de locale' }).click();
+      await expect(page.getByTestId('clear')).toBeVisible();
+      await expect(page.getByPlaceholder('Search')).toHaveValue('Jeans');
 
-    await page.getByRole('link', { name: 'en locale English' }).click();
-    await expect(page).toHaveURL('/');
+      await page.getByTestId('clear').click();
 
-    await expect(page.getByRole('link', { name: 'Help' })).toBeVisible();
-    await expect(page.getByPlaceholder('Search')).toBeEditable();
-    await expect(page.getByRole('link', { name: 'Kids' })).toBeVisible();
-  });
+      await expect(page.getByTestId('clear')).not.toBeVisible();
+      await expect(page.getByPlaceholder('Search')).toBeEmpty();
+    });
 
-  test('should clear search box when clicked the clear icon', async ({
-    page,
-  }) => {
-    await page.getByPlaceholder('Search').fill('Jeans');
+    test('Clicking the logo should redirect to homepage', async ({ page }) => {
+      await testLink(page, 'logo', '/');
+    });
 
-    await expect(page.getByTestId('clear')).toBeVisible();
-    await expect(page.getByPlaceholder('Search')).toHaveValue('Jeans');
-
-    await page.getByTestId('clear').click();
-
-    await expect(page.getByTestId('clear')).not.toBeVisible();
-    await expect(page.getByPlaceholder('Search')).toBeEmpty();
-  });
-
-  test('should navigate to homepage when clicked the logo', async ({
-    page,
-  }) => {
-    await page.getByRole('link', { name: 'logo' }).click();
-
-    await expect(page).toHaveURL('/');
-  });
-
-  test('should navigate to the clicked menu item page', async ({ page }) => {
-    await page.getByRole('link', { name: 'Men' }).click();
-
-    await expect(page).toHaveURL('/men');
+    test('Menu items should redirect to the correct page', async ({ page }) => {
+      for (const item of MENU_ITEMS) {
+        await testLink(page, item, item.toLowerCase());
+        await page.goBack();
+      }
+    });
   });
 
   test.describe('Mega Menu', () => {
@@ -63,23 +82,21 @@ test.describe('Header', () => {
       await page.getByRole('link', { name: 'Men' }).hover();
     });
 
-    test('should show mega menu when a menu item hovered', async ({ page }) => {
+    test('Mega menu should popup on menu item hover', async ({ page }) => {
       await expect(page.getByRole('link', { name: 'Shoes' })).toBeVisible();
       await expect(page.getByRole('link', { name: "All Men's" })).toBeVisible();
     });
 
-    test('should navigate to the clicked collection page', async ({ page }) => {
-      await page.getByRole('link', { name: 'Shoes' }).click();
-
-      await expect(page).toHaveURL('/men/shoes');
-    });
-
-    test('should navigate to the clicked subcollection page', async ({
+    test('Collections should redirect to the correct page', async ({
       page,
     }) => {
-      await page.getByRole('link', { name: 'Sneakers' }).click();
+      await testLink(page, 'Shoes', '/men/shoes');
+    });
 
-      await expect(page).toHaveURL('/men/shoes/sneakers');
+    test('SubCollections should redirect to the correct page', async ({
+      page,
+    }) => {
+      await testLink(page, 'Sneakers', '/men/shoes/sneakers');
     });
   });
 
@@ -87,3 +104,8 @@ test.describe('Header', () => {
    * test search box functionality
    */
 });
+
+const testLink = async (page: Page, name: string, URL: string) => {
+  await page.getByRole('link', { name }).click();
+  await expect(page).toHaveURL(URL);
+};
