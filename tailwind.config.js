cf808b89efed6ad9ff108c0f09832c3cd136b044
Mehrabmp <mehrabmpdev@gmail.com>
2022-11-03 20:30:36 +0330

    feat: change font to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Noto Sans', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
   },
