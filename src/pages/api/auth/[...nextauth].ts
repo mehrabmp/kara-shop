b1cbdc129b449c9babf46ebfa4760c86dd73f684
Mehrab <mehrabmpdev@gmail.com>
2022-11-07 10:33:02 +0330

    feat(auth): add authentication with next-auth (#133)
    * chore(deps): add next-auth and prisma adapter
    
    * feat(auth): add oAuth providers env variables example
    
    * feat(auth): add next-auth SessionProvider and api handler
    
    * feat(auth): add next-auth models to prisma schema

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from 'server/prisma';
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
