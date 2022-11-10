ff0628950f59e926485bb69514b9421e5f88826e
Mehrab <mehrabmpdev@gmail.com>
2022-11-10 16:05:27 +0330

    refactor: update animations on scroll (#146)

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -12,21 +12,21 @@ export const Hero = () => {
         <div className="flex flex-1 flex-col items-center justify-center pt-10 md:items-start md:px-4 md:pt-0">
           <span
             data-aos="fade-down"
-            data-aos-delay="700"
+            data-aos-delay="200"
             className="mb-2.5 rounded-md bg-violet-100 py-1 px-4 text-sm font-semibold text-violet-600 md:mb-5"
           >
             {t('hero.discount')}
           </span>
           <h2
             data-aos="fade-right"
-            data-aos-delay="700"
+            data-aos-delay="300"
             className="mb-5 text-center text-[2.5rem] font-bold leading-tight text-black md:text-left md:text-5xl"
           >
             {t('hero.title')}
           </h2>
           <h3
             data-aos="fade-right"
-            data-aos-delay="700"
+            data-aos-delay="400"
             className="font-regular mb-5 text-center text-lg leading-tight text-neutral-700 md:mb-10 md:text-left"
           >
             {t('hero.description')}
@@ -34,7 +34,7 @@ export const Hero = () => {
           <Link
             href={''}
             data-aos="fade-up"
-            data-aos-delay="700"
+            data-aos-delay="500"
             className="mb-10 flex items-center rounded bg-zinc-900 py-2.5 px-8 text-base font-normal text-white shadow-sm shadow-zinc-500"
           >
             <FiShoppingCart />
@@ -50,8 +50,8 @@ export const Hero = () => {
                 width={0}
                 height={0}
                 className={'mx-4 my-1 h-auto w-auto'}
-                data-aos="flip-right"
-                data-aos-delay="700"
+                data-aos="fade"
+                data-aos-delay="600"
               />
             ))}
           </div>
@@ -65,7 +65,6 @@ export const Hero = () => {
             width={550}
             height={550}
             data-aos="fade-up"
-            data-aos-delay="300"
           />
         </div>
       </div>
