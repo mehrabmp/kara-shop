39274c15c142db5339e13a131d9a3fc42eed8f2f
Mehrabmp <mehrabmpdev@gmail.com>
2022-11-17 16:26:07 +0330

    feat(ui): update Accordion component

--- src/components/Ui/Accordion/Accordion.tsx
+++ src/components/Ui/Accordion/Accordion.tsx
@@ -5,27 +5,29 @@ import { FiChevronUp } from 'react-icons/fi';
 
 interface Props {
   name: string;
+  open?: boolean;
   children?: React.ReactNode;
 }
 
-export const Accordion = ({ name, children }: Props) => {
-  const [isOpen, setIsOpen] = useState(false);
+export const Accordion = ({ name, open, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(open);
 
   return (
     <>
       <button
-        className={`flex w-full justify-between rounded-lg px-4 py-2.5 text-left text-sm font-medium text-neutral-900 outline-none transition hover:bg-neutral-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-700 focus-visible:ring-opacity-75`}
+        className={`flex w-full justify-between rounded-lg px-4 py-2.5 text-left text-sm font-semibold text-neutral-600 outline-none transition focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-700 focus-visible:ring-opacity-75`}
         onClick={() => setIsOpen(prev => !prev)}
       >
         <span>{name}</span>
         <FiChevronUp
-          className={clsx('h-5 w-5 transition duration-150', {
+          size="1rem"
+          className={clsx('transition duration-200', {
             'rotate-180 transform': isOpen,
           })}
         />
       </button>
-      <AnimateHeight duration={150} height={isOpen ? 'auto' : 0} animateOpacity>
-        <div className="px-4 py-2">{children}</div>
+      <AnimateHeight duration={200} height={isOpen ? 'auto' : 0} animateOpacity>
+        <div className="px-4 pb-3">{children}</div>
       </AnimateHeight>
     </>
   );
