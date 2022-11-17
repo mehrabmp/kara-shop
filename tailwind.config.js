26874a4502e8f1e0305a1a9ae42e240e89d4d795
Mehrab <mehrabmpdev@gmail.com>
2022-11-17 15:25:53 +0330

    feat: add tailwinds plugins (#167)

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,10 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
