c152b35089801bf4020393b5e7cec2ba3f43666b
Mehrabmp <mehrabmpdev@gmail.com>
2022-11-20 12:44:08 +0330

    refactor(ui): update CollectionsPage component

--- src/components/Layouts/BottomNavigation/CollectionsPage.tsx
+++ src/components/Layouts/BottomNavigation/CollectionsPage.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
-import { Disclosure, Transition } from '@headlessui/react';
 import { useTranslation } from 'next-i18next';
-import { FiChevronDown, FiX } from 'react-icons/fi';
+import { FiX } from 'react-icons/fi';
 import { Collections } from 'types';
 import { NavLink } from 'components/Layouts/Header/Header';
 import { convertToSlug } from 'utils';
+import { Accordion } from 'components';
 
 interface Props {
   navLinks: NavLink[];
@@ -32,89 +32,58 @@ export const CollectionsPage = ({
       </div>
       <ul className="mt-5 flex flex-col px-2">
         {navLinks.map((item, index) => (
-          <li key={index} className="font-medium text-neutral-800">
+          <li
+            key={index}
+            className="border-b border-solid border-neutral-100 font-medium text-neutral-800"
+          >
             {item.collapsible ? (
-              <Disclosure>
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button className="flex w-full items-center justify-between border-b border-solid border-neutral-100 py-4">
-                      <span>{t(`header:${item.name}`)}</span>
-                      <FiChevronDown
-                        className={`transition-transform duration-200 ${
-                          open ? 'rotate-180 transform' : ''
-                        }`}
-                      />
-                    </Disclosure.Button>
-                    <Transition
-                      enter="transition duration-100 ease-out"
-                      enterFrom="transform scale-95 opacity-0"
-                      enterTo="transform scale-100 opacity-100"
-                      leave="transition duration-75 ease-out"
-                      leaveFrom="transform scale-100 opacity-100"
-                      leaveTo="transform scale-95 opacity-0"
-                    >
-                      <Disclosure.Panel className="px-2 text-sm">
-                        <ul>
-                          {collections?.[
-                            item.name === 'men' ? 'men' : 'women'
-                          ].map(collection => (
-                            <Disclosure key={collection.id}>
-                              {({ open }) => (
-                                <>
-                                  <Disclosure.Button className="flex w-full items-center justify-between border-b border-solid border-neutral-100 py-4">
-                                    <span>{collection.title}</span>
-                                    <FiChevronDown
-                                      className={`transition-transform duration-200 ${
-                                        open ? 'rotate-180 transform' : ''
-                                      }`}
-                                    />
-                                  </Disclosure.Button>
-                                  <Transition
-                                    enter="transition duration-100 ease-out"
-                                    enterFrom="transform scale-95 opacity-0"
-                                    enterTo="transform scale-100 opacity-100"
-                                    leave="transition duration-75 ease-out"
-                                    leaveFrom="transform scale-100 opacity-100"
-                                    leaveTo="transform scale-95 opacity-0"
-                                  >
-                                    <Disclosure.Panel className="px-2 text-xs text-neutral-700">
-                                      <ul>
-                                        {collection.subCollections.map(
-                                          subCollection => (
-                                            <li key={subCollection.id}>
-                                              <Link
-                                                href={`${
-                                                  item.name
-                                                }/${convertToSlug(
-                                                  collection.title
-                                                )}/${convertToSlug(
-                                                  subCollection.title
-                                                )}`}
-                                                className="block border-b border-solid border-neutral-100 py-2"
-                                              >
-                                                <h3>{subCollection.title}</h3>
-                                              </Link>
-                                            </li>
-                                          )
-                                        )}
-                                      </ul>
-                                    </Disclosure.Panel>
-                                  </Transition>
-                                </>
-                              )}
-                            </Disclosure>
-                          ))}
-                        </ul>
-                      </Disclosure.Panel>
-                    </Transition>
-                  </>
-                )}
-              </Disclosure>
+              <Accordion>
+                <Accordion.Header>{t(`header:${item.name}`)}</Accordion.Header>
+                <Accordion.Body className="px-2 text-sm">
+                  <ul>
+                    {collections?.[item.name === 'men' ? 'men' : 'women'].map(
+                      collection => (
+                        <li
+                          key={collection.id}
+                          className="block border-b border-solid border-neutral-100"
+                        >
+                          <Accordion>
+                            <Accordion.Header>
+                              {collection.title}
+                            </Accordion.Header>
+                            <Accordion.Body className="px-2 text-xs">
+                              <ul>
+                                {collection.subCollections.map(
+                                  subCollection => (
+                                    <li
+                                      key={subCollection.id}
+                                      className="block border-b border-solid border-neutral-100 py-2"
+                                    >
+                                      <Link
+                                        href={`/products/${
+                                          item.name
+                                        }/${convertToSlug(
+                                          collection.title
+                                        )}/${convertToSlug(
+                                          subCollection.title
+                                        )}`}
+                                      >
+                                        <h3>{subCollection.title}</h3>
+                                      </Link>
+                                    </li>
+                                  )
+                                )}
+                              </ul>
+                            </Accordion.Body>
+                          </Accordion>
+                        </li>
+                      )
+                    )}
+                  </ul>
+                </Accordion.Body>
+              </Accordion>
             ) : (
-              <Link
-                href={item.name}
-                className="block border-b border-solid border-neutral-100 py-4"
-              >
+              <Link href={item.name} className="block py-4">
                 <h3>{t(`header:${item.name}`)}</h3>
               </Link>
             )}
