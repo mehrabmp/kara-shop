3961cd78082418f03920d56c23666ede299bc232
Mehrabmp <mehrabmpdev@gmail.com>
2022-12-14 16:19:41 +0330

    refactor: update trpc context

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,30 +1,38 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import * as trpc from '@trpc/server';
-import * as trpcNext from '@trpc/server/adapters/next';
+import { type inferAsyncReturnType } from '@trpc/server';
+import { type CreateNextContextOptions } from '@trpc/server/adapters/next';
+import { type Session } from 'next-auth';
+import { getServerAuthSession } from '../common/get-server-auth-session';
+import { prisma } from '../db/prisma';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface CreateContextOptions {
-  // session: Session | null
-}
+type CreateContextOptions = {
+  session: Session | null;
+};
+
+/** Use this helper for:
+ * - testing, so we dont have to mock Next.js' req/res
+ * - trpc's `createSSGHelpers` where we don't have req/res
+ * @see https://create.t3.gg/en/usage/trpc#-servertrpccontextts
+ **/
+export const createContextInner = async (opts: CreateContextOptions) => {
+  return {
+    session: opts.session,
+    prisma,
+  };
+};
 
 /**
- * Inner function for `createContext` where we create the context.
- * This is useful for testing when we don't want to mock Next.js' request/response
- */
-export async function createContextInner(_opts: CreateContextOptions) {
-  return {};
-}
-
-export type Context = trpc.inferAsyncReturnType<typeof createContextInner>;
-
-/**
- * Creates context for an incoming request
+ * This is the actual context you'll use in your router
  * @link https://trpc.io/docs/context
- */
-export async function createContext(
-  opts: trpcNext.CreateNextContextOptions
-): Promise<Context> {
-  // for API-response caching see https://trpc.io/docs/caching
+ **/
+export const createContext = async (opts: CreateNextContextOptions) => {
+  const { req, res } = opts;
 
-  return await createContextInner({});
-}
+  // Get the session from the server using the unstable_getServerSession wrapper function
+  const session = await getServerAuthSession({ req, res });
+
+  return await createContextInner({
+    session,
+  });
+};
+
+export type Context = inferAsyncReturnType<typeof createContext>;
