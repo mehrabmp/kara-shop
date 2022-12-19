f8a9a3888d54d0c40d2c82a5d2ae1c818cc14366
Mehrabmp <mehrabmpdev@gmail.com>
2022-12-19 19:49:11 +0330

    feat(ui): create ProductItem component

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,3 +8,6 @@ export const convertToSlug = (text: string) => {
 export const capitalizeFirstLetter = (text: string) => {
   return text.charAt(0).toUpperCase() + text.slice(1);
 };
+
+export const numberWithCommas = (number: number | string) =>
+  number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
