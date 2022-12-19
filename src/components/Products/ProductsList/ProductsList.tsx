922ae2b59ba33086f024d4746e59292406340853
Mehrabmp <mehrabmpdev@gmail.com>
2022-12-19 19:50:14 +0330

    feat: fetch products and show them

--- src/components/Products/ProductsList/ProductsList.tsx
+++ src/components/Products/ProductsList/ProductsList.tsx
@@ -1,5 +1,19 @@
 import Link from 'next/link';
+import { Product } from 'types';
+import { ProductItem } from '../ProductItem/ProductItem';
 
-export const ProductsList = () => {
-  return <div className="h-[500px] rounded-lg bg-white"></div>;
+interface Props {
+  products: Product[];
+}
+
+export const ProductsList = ({ products }: Props) => {
+  return (
+    <div className="rounded-lg bg-white">
+      <div className="grid grid-cols-4 gap-5 p-4">
+        {products.map(product => (
+          <ProductItem key={product.id} {...product} />
+        ))}
+      </div>
+    </div>
+  );
 };
