97c7983dc17ded2d65ef83423d475b64246d64bf
Mehrab <mehrabmpdev@gmail.com>
2022-12-20 17:05:38 +0330

    feat(ui): make the products list page responsive (#217)
    * refactor(ui): update ProductItem component
    
    * feat(ui): make products listing page responsive

--- src/components/Products/ProductItem/ProductItem.tsx
+++ src/components/Products/ProductItem/ProductItem.tsx
@@ -18,16 +18,20 @@ export const ProductItem = ({
 }: Product) => {
   const [currentImage, setCurrentImage] = useState(images[0]);
 
+  const productLink = `/product/${id}/slug`;
+
   return (
-    <Link href={`/product/${id}/`} className="group overflow-hidden">
+    <div className="group overflow-hidden">
       <div className="relative h-[350px] w-full overflow-hidden rounded-lg transition">
-        <Image
-          src={currentImage}
-          alt={`${title} image`}
-          className="duration-[2s] group-hover:scale-110"
-          fill
-        />
-        <div className="h-full w-full bg-black opacity-0 duration-500 group-hover:opacity-10"></div>
+        <Link href={productLink}>
+          <Image
+            src={currentImage}
+            alt={`${title} image`}
+            className="duration-[2s] group-hover:scale-110"
+            fill
+          />
+          <div className="h-full w-full bg-black opacity-0 duration-500 group-hover:opacity-10"></div>
+        </Link>
         <button
           className="absolute top-3 right-3 z-10 hidden rounded-xl bg-white p-2.5 text-lg group-hover:block"
           onClick={() => console.log('wishlist')}
@@ -60,19 +64,21 @@ export const ProductItem = ({
           </button>
         ))}
       </div>
-      <h2 className="text-base font-medium">{title}</h2>
-      <h3 className="text-sm font-normal capitalize text-neutral-500">
-        {type.toString().toLowerCase()} {subCollection.title}
-      </h3>
-      <div className="flex items-center justify-between">
-        <h3 className="my-3 text-lg font-medium text-black">
-          ${numberWithCommas(price.toFixed(2))}
+      <Link href={productLink}>
+        <h2 className="text-base font-medium">{title}</h2>
+        <h3 className="py-1 text-xs font-normal capitalize text-neutral-500">
+          {type.toString().toLowerCase()} {subCollection.title}
         </h3>
-        <div className="flex items-center justify-center text-sm text-neutral-500">
-          <BsStarFill className="mr-1 text-yellow-400" />
-          <h4>{rate} (69 Reviews)</h4>
+        <div className="flex items-center justify-between">
+          <h3 className="my-3 text-xl font-medium text-black">
+            ${numberWithCommas(price.toFixed(2))}
+          </h3>
+          <div className="flex items-center justify-center text-xs text-neutral-500">
+            <BsStarFill className="mr-1 text-yellow-400" />
+            <h4>{rate} (69 Reviews)</h4>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
