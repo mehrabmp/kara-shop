1847411e2b23e926998aae7aec30f294f919ef4b
Mehrab <mehrabmpdev@gmail.com>
2023-01-20 22:10:58 +0330

    feat(ui): update announcement bar animation (#244)

--- src/components/Layouts/Header/AnnouncementBar.tsx
+++ src/components/Layouts/Header/AnnouncementBar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import AnimateHeight from 'react-animate-height';
 import Countdown from 'react-countdown';
 import { FiChevronRight, FiX } from 'react-icons/fi';
 
@@ -8,7 +9,7 @@ const AnnouncementBar = () => {
   const [isBarHovered, setIsBarHovered] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => setIsBarOpen(true), 1000);
+    setTimeout(() => setIsBarOpen(true), 2000);
   }, []);
 
   const epochMs = new Date(2022, 0).valueOf();
@@ -18,29 +19,29 @@ const AnnouncementBar = () => {
   const nextday = (index + 1) * msInDay + epochMs;
 
   return (
-    <div
-      className={`relative flex items-center justify-center overflow-hidden bg-[#e91e63] text-[10px] font-normal text-white transition-all duration-300 md:text-sm ${
-        isBarOpen ? 'h-8 md:h-9' : 'h-0'
-      }`}
-    >
-      <Link
-        href={'/off'}
-        className="flex h-full w-full items-center justify-center p-2"
-        onMouseEnter={() => setIsBarHovered(true)}
-        onMouseLeave={() => setIsBarHovered(false)}
-      >
-        <h4 className="mr-1">
-          Today deal sale off <b>50%</b>. End in{' '}
-          {<b>{<Countdown date={nextday} />}</b>}. Shop Now
-        </h4>
-        <FiChevronRight
-          className={`transition-all duration-200 ${isBarHovered && 'ml-1.5'}`}
-        />
-      </Link>
-      <div className="absolute right-0 top-0 bottom-0 flex cursor-pointer items-center justify-center px-2">
-        <FiX size="1.25rem" onClick={() => setIsBarOpen(false)} />
+    <AnimateHeight duration={300} height={isBarOpen ? 'auto' : 0}>
+      <div className="relative flex h-8 items-center justify-center overflow-hidden bg-[#e91e63] text-[10px] font-normal text-white transition-all duration-300 md:h-9 md:text-sm">
+        <Link
+          href={'/off'}
+          className="flex h-full w-full items-center justify-center p-2"
+          onMouseEnter={() => setIsBarHovered(true)}
+          onMouseLeave={() => setIsBarHovered(false)}
+        >
+          <h4 className="mr-1">
+            Today deal sale off <b>50%</b>. End in{' '}
+            {<b>{<Countdown date={nextday} />}</b>}. Shop Now
+          </h4>
+          <FiChevronRight
+            className={`transition-all duration-200 ${
+              isBarHovered && 'ml-1.5'
+            }`}
+          />
+        </Link>
+        <div className="absolute right-0 top-0 bottom-0 flex cursor-pointer items-center justify-center px-2">
+          <FiX size="1.25rem" onClick={() => setIsBarOpen(false)} />
+        </div>
       </div>
-    </div>
+    </AnimateHeight>
   );
 };
 
