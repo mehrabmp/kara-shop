d61a295b9b9fc1574a6589ef8badd006927be2b8
Mehrabmp <mehrabmpdev@gmail.com>
2023-04-01 22:54:39 +0330

    chore(deps): update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
   ],
 };
