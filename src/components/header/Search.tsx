f5de46fdcd4fee6c7b5dc83c0c32d0a2725fadf7
Mehrab <mehrabmpdev@gmail.com>
2023-04-23 13:10:00 +0330

    feat(ui): make Search component accessible

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { FiSearch, FiX } from 'react-icons/fi';
 import { useTranslation } from 'next-i18next';
 
@@ -8,9 +8,7 @@ interface Props {
 
 export const Search = ({ onSearch }: Props) => {
   const [value, setValue] = useState('');
-
   const inputRef = useRef<HTMLInputElement>(null);
-
   const { t } = useTranslation('common');
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -23,6 +21,8 @@ export const Search = ({ onSearch }: Props) => {
     if (inputRef.current) inputRef.current.focus();
   };
 
+  const showClearButton = !!value;
+
   return (
     <form
       className="relative flex h-10 max-w-[200px] content-between items-center"
@@ -34,18 +34,28 @@ export const Search = ({ onSearch }: Props) => {
         name="search"
         id="search"
         placeholder={`${t('search')}`}
+        aria-label="Search"
         value={value}
         ref={inputRef}
         onChange={e => setValue(e.target.value)}
       />
-      {value ? (
-        <FiX
+      {showClearButton ? (
+        <button
+          type="button"
           className="absolute right-0 h-full w-[30px] cursor-pointer pr-2.5 text-neutral-500"
-          data-testid="clear"
+          aria-label="Clear search input"
           onClick={handleClearInput}
-        />
+        >
+          <FiX size="1.25rem" />
+        </button>
       ) : (
-        <FiSearch className="absolute right-0 h-full w-[30px] cursor-pointer pr-2.5 text-neutral-500" />
+        <button
+          type="submit"
+          className="absolute right-0 h-full w-[30px] cursor-pointer pr-2.5 text-neutral-500"
+          aria-label="Submit search"
+        >
+          <FiSearch size="1.25rem" />
+        </button>
       )}
     </form>
   );
