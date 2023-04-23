7e5b5e4e21a6f8852f5b2fd46cde27301538ea43
Mehrab <mehrabmpdev@gmail.com>
2023-04-23 12:32:43 +0330

    refactor: clean up TopBar component codes (#306)
    This commit refactors the Topbar component by extracting the top bar item into its own TopbarItem component. This simplifies the code and makes it more readable

--- src/components/header/TopBar.tsx
+++ src/components/header/TopBar.tsx
@@ -1,55 +1,63 @@
-import React, { useRef, useState } from 'react';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
-import { FiPhone, FiGrid, FiChevronDown } from 'react-icons/fi';
-import { LocaleSelector } from './LocaleSelector';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useRef, useState } from 'react';
+import type { IconType } from 'react-icons';
+import { FiChevronDown, FiGrid, FiPhone } from 'react-icons/fi';
 import { useClickAway } from 'react-use';
-import { IconType } from 'react-icons';
+import { LocaleSelector } from './LocaleSelector';
+
+interface TopbarItemProps {
+  label: string;
+  url: string;
+  Icon?: IconType;
+}
+
+const TopbarItem = ({ label, url, Icon }: TopbarItemProps) => (
+  <li className="mx-1 pb-px md:mr-2.5 lg:[&:nth-of-type(3)]:mr-10 lg:[&:nth-of-type(5)]:mr-10">
+    <Link
+      href={url}
+      className="flex items-center transition-colors hover:text-white"
+    >
+      {Icon && <Icon className="mx-1 md:text-sm"></Icon>}
+      <span>{label}</span>
+    </Link>
+  </li>
+);
 
 export const TopBar = () => {
-  const [isChangeLocaleOpen, setIsChangeLocaleOpen] = useState(false);
-
+  const [isLocaleSelectorOpen, setIsLocaleSelectorOpen] = useState(false);
   const router = useRouter();
-
   const { t } = useTranslation('header');
-
-  const topbarItems: [string, string, IconType?][] = [
-    [t('topbar.careers'), 'careers'],
-    [t('topbar.help'), 'help'],
-    [t('topbar.buyer'), 'buyer'],
-    [t('topbar.download'), 'https://play.google.com/store/apps', FiGrid],
-    [t('topbar.phone'), 'tel:+0125258192502', FiPhone],
-  ];
-
   const ref = useRef<HTMLDivElement>(null);
 
-  useClickAway(ref, () => setIsChangeLocaleOpen(false));
+  useClickAway(ref, () => setIsLocaleSelectorOpen(false));
+
+  const topbarItems: TopbarItemProps[] = [
+    { label: t('topbar.careers'), url: 'careers' },
+    { label: t('topbar.help'), url: 'help' },
+    { label: t('topbar.buyer'), url: 'buyer' },
+    {
+      label: t('topbar.download'),
+      url: 'https://play.google.com/store/apps',
+      Icon: FiGrid,
+    },
+    { label: t('topbar.phone'), url: 'tel:+0125258192502', Icon: FiPhone },
+  ];
 
   return (
     <div className="bg-[#232323] text-[10px] text-gray-300 md:text-xs">
       <div className="mx-auto flex flex-col items-center px-4 py-1 xl:container md:flex-row md:py-2.5">
         <p className="pb-2 md:pb-0">{t('topbar.discount')}</p>
         <ul className="flex flex-wrap justify-center md:ml-auto">
-          {topbarItems.map(([label, url, Icon]) => (
-            <li
-              key={label}
-              className="mx-1 pb-px md:mr-2.5 lg:[&:nth-of-type(3)]:mr-10 lg:[&:nth-of-type(5)]:mr-10"
-            >
-              <Link
-                href={url}
-                className="flex items-center transition-colors hover:text-white"
-              >
-                {Icon && <Icon className="mx-1 md:text-sm"></Icon>}
-                <span>{label}</span>
-              </Link>
-            </li>
+          {topbarItems.map(item => (
+            <TopbarItem key={item.label} {...item} />
           ))}
           <div
             className="relative z-50 ml-2.5 flex cursor-pointer items-center"
             ref={ref}
-            onClick={() => setIsChangeLocaleOpen(prev => !prev)}
+            onClick={() => setIsLocaleSelectorOpen(prev => !prev)}
           >
             <div className="relative mr-1.5 h-3.5 w-3.5 md:h-[17px] md:w-[17px]">
               <Image
@@ -61,7 +69,7 @@ export const TopBar = () => {
             </div>
             <span>{router.locale?.toUpperCase()}</span>
             <FiChevronDown color="#fff"></FiChevronDown>
-            <LocaleSelector isOpen={isChangeLocaleOpen} />
+            <LocaleSelector isOpen={isLocaleSelectorOpen} />
           </div>
         </ul>
       </div>
